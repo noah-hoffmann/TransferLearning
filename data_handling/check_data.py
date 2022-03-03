f430a22f9fc416da9ddaf93a07989f227d369dd5
Noah Hoffmann <noahhoffmann1202@gmail.com>
2022-03-03 10:19:41 +0100

    changed check

--- data_handling/check_data.py
+++ data_handling/check_data.py
@@ -9,12 +9,16 @@ from tqdm import tqdm
 def main():
     data_dir = 'data'
     files = glob(os.path.join(data_dir, '*.pickle.gz'))
+    total = 0
+    missing = 0
     for file in tqdm(files):
         with gz.open(file) as f:
             data: list[ComputedStructureEntry] = pickle.load(f)
+        total += len(data)
         for entry in data:
             if entry.data['e_above_hull'] is None:
-                print(f"\n Entry in file {file!r} with mat_id {entry.data['id']!r} has missing 'e_above_hull'!")
+                missing += 1
+    print(f'There are {missing} / {total} missing e_above_hull values!')
 
 
 if __name__ == '__main__':
