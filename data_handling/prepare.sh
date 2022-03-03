52329c9af970862273224ff58383b9dfe0732409
Noah Hoffmann <noahhoffmann1202@gmail.com>
2022-03-03 18:53:18 +0100

    aadded file

--- data_handling/prepare.sh
+++ data_handling/prepare.sh
@@ -1,12 +1,12 @@
 for file in split_data/*.pickle.gz
 do
-  prepare --source-dir . --target-dir prepared &
+  prepare --source-dir . --target-dir prepared --file $file &
 done
 for file in split_data/val/*.pickle.gz
 do
-  prepare --source-dir . --target-dir prepared/val &
+  prepare --source-dir . --target-dir prepared/val --file $file &
 done
 for file in split_data/test/*.pickle.gz
 do
-  prepare --source-dir . --target-dir prepared/test &
+  prepare --source-dir . --target-dir prepared/test --file $file &
 done
