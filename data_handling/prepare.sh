09bacdf3e6287eee58d3abf9cf8126ebcd7f85cb
Noah Hoffmann <noahhoffmann1202@gmail.com>
2022-03-04 14:45:19 +0100

    added checking for directory existence in prepare.sh

--- data_handling/prepare.sh
+++ data_handling/prepare.sh
@@ -1,12 +1,30 @@
 path=$1;
+
+if [ ! -d "$path"/prepared ]
+  then
+    mkdir "$path"/prepared
+fi
+
 for file in "$path"/split/*.pickle.gz
 do
   prepare --source-dir . --target-dir "$path"/prepared --file "$file" &
 done
+
+if [ ! -d "$path"/prepared/val ]
+  then
+    mkdir "$path"/prepared/val
+fi
+
 for file in "$path"/split/val/*.pickle.gz
 do
   prepare --source-dir . --target-dir "$path"/prepared/val --file "$file" &
 done
+
+if [ ! -d "$path"/prepared/test ]
+  then
+    mkdir "$path"/prepared/test
+fi
+
 for file in "$path"/split/test/*.pickle.gz
 do
   prepare --source-dir . --target-dir "$path"/prepared/test --file "$file" &
