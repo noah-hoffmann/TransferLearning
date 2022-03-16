d11eeb597b7429e0ff1d48c664e6934a9573d0ad
Noah Hoffmann <noahhoffmann1202@gmail.com>
2022-03-16 15:22:38 +0100

    refined checking

--- data_handling/find_leakage.py
+++ data_handling/find_leakage.py
@@ -6,23 +6,33 @@ from tqdm import tqdm
 def main():
     path = os.path.join('pbe', 'prepared', '*', '*.pickle.gz')
     files = get_files(path)
-    ids = set()
+    val_test_ids = set()
     for file in tqdm(files):
         data = load(file)
         for batch_id, in data['batch_ids']:
-            ids.add(batch_id)
+            val_test_ids.add(batch_id)
+
+    train_ids = set()
+    for file in tqdm(get_files(os.path.join('pbe', 'prepared', '*.pickle.gz'))):
+        data = load(file)
+        for batch_id, in data['batch_ids']:
+            train_ids.add(batch_id)
 
     to_check = get_files(os.path.join('scan', 'prepared', '*', '*.pickle.gz'))
     # get_files(os.path.join('ps', 'prepared', '*', '*.pickle.gz'))
     total = 0
-    remaining = 0
+    in_val_test = 0
+    in_training = 0
     for file in tqdm(to_check):
         data = load(file)
         total += len(data['batch_ids'])
         for batch_id, in data['batch_ids']:
-            if batch_id in ids:
-                remaining += 1
-    print(f"{remaining} / {total} = {remaining / total:.2%}")
+            if batch_id in val_test_ids:
+                in_val_test += 1
+            elif batch_id in train_ids:
+                in_training += 1
+    print(f"{in_val_test / total:.2f} are also in pbe test or val set.")
+    print(f"{in_training/ total:.2f} are also in pbe training set.")
 
 
 if __name__ == '__main__':
