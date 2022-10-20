df8ccb7b95c10f2876ecbb8d91345312545d3782
Noah Hoffmann <noahhoffmann1202@gmail.com>
2022-10-20 15:00:22 +0200

    test for removing entries with misssing property

---
+++ data_handling/removing_missing_property.py
@@ -0,0 +1,31 @@
+from tqdm import tqdm
+from glob import glob
+import os.path
+from argparse import ArgumentParser
+from util import load, save
+from pymatgen.entries.computed_entries import ComputedStructureEntry
+
+
+def main():
+    parser = ArgumentParser()
+    parser.add_argument('--data-dir', '-d')
+    parser.add_argument('--property', '-p')
+
+    args = parser.parse_args()
+
+    files = glob(os.path.join(args.data_dir, '*.pickle.gz')) + glob(os.path.join(args.data_dir, '*', '*.pickle.gz'))
+
+    for file in tqdm(files):
+        data: list[ComputedStructureEntry] = load(file)
+        to_remove = []
+        for i, entry in enumerate(data):
+            if args.property not in entry.data:
+                to_remove.append(i)
+
+        for i in reversed(to_remove):
+            data.pop(i)
+        print(f"{len(to_remove)} entries are removed!")
+
+
+if __name__ == '__main__':
+    main()
